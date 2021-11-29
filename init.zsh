891238d62012378f7e312ad6c8759a291df2de9c
Hanley Lee <hanley.lei@gmail.com>
2021-11-29 23:48:28 +0800

    daily 2021-11-29

--- init.zsh
+++ init.zsh
@@ -26,7 +26,10 @@ export HKMS="$HL_REPO/hkms"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
-export LANG=en_CN.UTF8
+# export LANG=en_CN.UTF8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 export ROOTMARKERS=(
     ".svn"
