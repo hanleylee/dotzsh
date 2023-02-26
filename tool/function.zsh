99fb1fffb1f93bd856b2a1929616513ac0d3e53a
Hanley Lee <hanley.lei@gmail.com>
2023-02-26 23:27:30 +0800

    daily 2023-02-26

--- tool/function.zsh
+++ tool/function.zsh
@@ -184,8 +184,8 @@ function _zi {
 
 # *************** fzf-git *****************
 _fzf_git_fzf() {
-    fzf-tmux -p80%,80% -- \
-        --layout=reverse --multi --height=80% --min-height=20 --border \
+    fzf-tmux -p90%,90% -- \
+        --layout=reverse --multi --height=90% --min-height=20 --border \
         --color='header:italic:underline' \
         --preview-window='right,50%,border-left' \
         --bind='ctrl-/:change-preview-window(down,50%,border-top|hidden|)' "$@"
