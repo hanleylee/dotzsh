a7c72bbb9289dc13ae02e08c13cd6445f149e686
Hanley Lee <hanley.lei@gmail.com>
2023-04-01 12:14:09 +0800

    daily 2023-04-01

--- init_p2.zsh
+++ init_p2.zsh
@@ -28,6 +28,7 @@ _path_arr+=(
     "$XDG_CONFIG_HOME/broot/launcher/bash/br"
     "$HOMEBREW_PREFIX/etc/profile.d/autojump.sh"
     "$HOMEBREW_PREFIX/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+    "$HOME/.orbstack/shell/init.zsh"
 )
 if [[ $TERM_PROGRAM == "iTerm.app" ]] && [[ -d "$ZDOTDIR/.iterm2_shell_integration.zsh" ]]; then
     _path_arr+=("$ZDOTDIR/.iterm2_shell_integration.zsh")
