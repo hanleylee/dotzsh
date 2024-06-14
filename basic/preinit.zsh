9ed047d2c3f2b49de90a3e22201c295e69e4f895
Hanley Lee <hanley.lei@gmail.com>
2024-06-14 18:34:09 +0800

    daily 2024-06-14

--- basic/preinit.zsh
+++ basic/preinit.zsh
@@ -149,6 +149,7 @@ function remove_element_if_path_not_exist() {
 #***************   Homebrew   *****************
 if command_exists brew; then
     export HOMEBREW_NO_AUTO_UPDATE=true # 禁用 Homebrew 每次安装软件时的更新
+    export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=true
     # export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
     # export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
     # export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
