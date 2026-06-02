b458ac276e3488cbe15f5d0bc0923a7b73d271b7
Hanley Lee <hanley.lei@gmail.com>
2026-06-02 17:30:59 +0800

    daily 2026-06-02

--- main/alias.zsh
+++ main/alias.zsh
@@ -66,6 +66,7 @@ command_exists trash && alias rm='trash'
 command_exists gkill && alias kill='gkill'
 command_exists gls && alias ls='gls --color=tty'
 if command_exists eza; then alias l='eza -laghHimU --git --group-directories-first --icons -F'; else alias l='ls -lhia'; fi
+if command_exists eza; then alias ll='eza -laghHimU --git --group-directories-first --icons -F'; else alias ll='ls -lhia'; fi
 # [[ -d "$HOME/.hlconfig.git" ]] && alias hlconfig='git --git-dir=$HOME/.hlconfig.git/ --work-tree=$HOME'
 
 # for homebrew {{{
